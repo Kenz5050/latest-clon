f216906f0fe5f97242199fa61eedd5dd70db1f28
Kenz5050 <142476883+Kenz5050@users.noreply.github.com>
2025-12-23 04:18:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+// alert ("Yyfyhhf")
+
+$(document) .ready(function () {
+    $("#l1") .on("click" , function() {
+console.log("clicked")
+    $("#p1") .toggle();
+    });
+});
+$(document) .ready(function () {
+    $("#l2") .on("click" , function() {
+console.log("clicked")
+    $("#p2") .toggle();
+    });
+});
+$(document) .ready(function () {
+    $("#l3") .on("click" , function() {
+console.log("clicked")
+    $("#p3") .toggle();
+    });
+});
+$(document) .ready(function () {
+    $("#l4") .on("click" , function() {
+console.log("clicked")
+    $("#p4") .toggle();
+    });
+});
+$(document) .ready(function () {
+    $("#l5") .on("click" , function() {
+console.log("clicked")
+    $("#p5") .toggle();
+    });
+});
+
+$(document) .ready(function () {
+    $("#l6") .on("click" , function() {
+console.log("clicked")
+    $("#p6") .toggle();
+    });
+});
+$(document) .ready(function () {
+    $("#l7") .on("click" , function() {
+console.log("clicked")
+    $("#p7") .toggle();
+    });
+});
+$(document) .ready(function () {
+    $("#l8") .on("click" , function() {
+console.log("clicked")
+    $("#p8") .toggle();
+    });
+});
+$(document) .ready(function () {
+    $("#l9") .on("click" , function() {
+console.log("clicked")
+    $("#p9") .toggle();
+    });
+});
+$(document) .ready(function () {
+    $("#l_10") .on("click" , function() {
+console.log("clicked")
+    $("#p_10") .toggle();
+    });
+});
+
+// $(document).ready(function () {
+//   $("#menu li").on("click", function () {
+//     const target = $(this).data("target");
+
+//     // Fade out all contents except the clicked one
+//     $(".colli").not(target).fadeOut(200);
+
+//     // Fade toggle the clicked one
+//     $(target).fadeToggle(200);
+//   });
+// });
+
+
+
+$(document) .ready(function () {
+    $("#investment_drop") .on("click" , function() {
+console.log("clicked")
+    $(".investment_drop_down") .toggle();
+    });
+});
+
+$(document) .ready(function () {
+    $(".header_three_line_button") .on("click" , function() {
+console.log("clicked")
+    $(".mobile_fade_toggle") .toggle();
+    });
+});
